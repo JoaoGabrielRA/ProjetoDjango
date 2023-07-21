853a8ef621fac8b1da4a82b75af1273954f0317e
joao.rodrigues <joao.rodrigues@natal.rn.gov.br>
2023-07-21 16:50:07 -0300

    First User registration Test

--- projeto_cadastro_usuarios/app_cadastro_usuarios/views.py
+++ projeto_cadastro_usuarios/app_cadastro_usuarios/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Usuario
+def home(request):
+    return render(request,'usuarios/home.html')
 
-# Create your views here.
+def usuarios(request):
+    #salva os dados do form no DB
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    #Exibe todos os users cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    #retorna os dados para a listagem
+
+    return render(request, 'usuarios/usuarios.html', usuarios)
